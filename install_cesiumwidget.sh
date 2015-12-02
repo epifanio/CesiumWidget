c790bcaa33a23757d51c79174dacf73a9777d25c
epifanio <epiesasha@me.com>
2015-12-02 17:03:45 -0500

    Update install_cesiumwidget.sh

--- install_cesiumwidget.sh
+++ install_cesiumwidget.sh
@@ -3,6 +3,6 @@ mkdir -p /home/main/src/
 git clone https://github.com/epifanio/CesiumWidget /home/main/src/CesiumWidget
 cd /home/main/src/CesiumWidget
 /home/main/anaconda/envs/python3/bin/python setup.py install
-python -m CesiumWidget.install --symlink --user --force
+/home/main/anaconda/envs/python3/bin/python -m CesiumWidget.install --symlink --user --force
 
 
