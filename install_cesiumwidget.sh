3a0e2d360fa96556fa4583aee98e8339eb9324d9
epifanio <epiesasha@me.com>
2015-12-02 13:39:14 -0500

    removed user parameter from install instruction

--- install_cesiumwidget.sh
+++ install_cesiumwidget.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/epifanio/CesiumWidget /tmp/CesiumWidget
 cd /tmp/CesiumWidget
-/home/main/anaconda/envs/python3/bin/python setup.py install user
+/home/main/anaconda/envs/python3/bin/python setup.py install
+# user
 
 
