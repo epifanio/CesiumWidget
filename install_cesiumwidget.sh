713ee385b1cdc40acd3816c249af880152db6fbe
epifanio <epiesasha@me.com>
2015-12-02 15:35:25 -0500

    refactoring the entire repository

--- install_cesiumwidget.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /home/main/src/
-git clone https://github.com/epifanio/CesiumWidget /home/main/src/CesiumWidget
-cd /home/main/src/CesiumWidget
-/home/main/anaconda/envs/python3/bin/python setup.py install
-python -m CesiumWidget.install --symlink --user --force
-
-
