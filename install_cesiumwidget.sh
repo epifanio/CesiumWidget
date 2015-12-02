ec8b70c43dab5a24a06e69d20ff13ab54475b395
epifanio <epiesasha@me.com>
2015-12-02 14:35:34 -0500

    refactoring the entire repository

--- install_cesiumwidget.sh
+++ install_cesiumwidget.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-
-git clone https://github.com/epifanio/CesiumWidget /tmp/CesiumWidget
-cd /tmp/CesiumWidget
-/home/main/anaconda/envs/python3/bin/python setup.py install all
-# user
+mkdir -p /home/main/src/
+git clone https://github.com/epifanio/CesiumWidget /home/main/src/CesiumWidget
+cd /home/main/src/CesiumWidget
+/home/main/anaconda/envs/python3/bin/python setup.py install
+python -m CesiumWidget.install --symlink --user --force
 
 
--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
+from __future__ import print_function
+from setuptools import setup, find_packages, Command
+from setuptools.command.egg_info import egg_info
+from subprocess import check_call
+import os
+import sys
+import platform
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+LONG_DESCRIPTION = """
 CesiumWidget
 ====
 
@@ -9,194 +18,108 @@ A Cesiumjs widget for the Ipython notebook, forked from https://github.com/petru
 .. _Cesiumjs: http://cesiumjs.org/
 """
 
-from distutils.core import setup
-import os
-import sys
-import subprocess
-from jupyter_core.paths import jupyter_config_dir, jupyter_data_dir
-import shutil
+try:
+    from shutil import which
+except ImportError:
+    # The which() function is copied from Python 3.4.3
+    # PSF license version 2 (Python Software Foundation License Version 2)
+    def which(cmd, mode=os.F_OK | os.X_OK, path=None):
+        """Given a command, mode, and a PATH string, return the path which
+        conforms to the given mode on the PATH, or None if there is no such
+        file.
+        `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+        of os.environ.get("PATH"), or can be overridden with a custom search
+        path.
+        """
+        # Check that a given file can be accessed with the correct mode.
+        # Additionally check that `file` is not a directory, as on Windows
+        # directories pass the os.access check.
+        def _access_check(fn, mode):
+            return (os.path.exists(fn) and os.access(fn, mode)
+                    and not os.path.isdir(fn))
 
-from os.path import expanduser
+        # If we're given a path with a directory part, look it up directly rather
+        # than referring to PATH directories. This includes checking relative to the
+        # current directory, e.g. ./script
+        if os.path.dirname(cmd):
+            if _access_check(cmd, mode):
+                return cmd
+            return None
 
-if sys.version_info[:2] < (2, 6) or (3, 0) <= sys.version_info[0:2] < (3, 2):
-    raise RuntimeError("Python version 2.6, 2.7 or >= 3.2 required.")
+        if path is None:
+            path = os.environ.get("PATH", os.defpath)
+        if not path:
+            return None
+        path = path.split(os.pathsep)
 
-if sys.version_info[0] >= 3:
-    import builtins
-else:
-    import __builtin__ as builtins
+        if sys.platform == "win32":
+            # The current directory takes precedence on Windows.
+            if not os.curdir in path:
+                path.insert(0, os.curdir)
 
-
-def recursive_overwrite(src, dest, ignore=None):
-    if os.path.isdir(src):
-        if not os.path.isdir(dest):
-            os.makedirs(dest)
-        files = os.listdir(src)
-        if ignore is not None:
-            ignored = ignore(src, files)
+            # PATHEXT is necessary to check on Windows.
+            pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+            # See if the given file matches any of the expected path extensions.
+            # This will allow us to short circuit when given "python.exe".
+            # If it does match, only test that one, otherwise we have to try
+            # others.
+            if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+                files = [cmd]
+            else:
+                files = [cmd + ext for ext in pathext]
         else:
-            ignored = set()
-        for f in files:
-            if f not in ignored:
-                recursive_overwrite(os.path.join(src, f),
-                                    os.path.join(dest, f),
-                                    ignore)
-    else:
-        shutil.copyfile(src, dest)
+            # On other platforms you don't have things like PATHEXT to tell you
+            # what file suffixes are executable, so just pass on cmd as-is.
+            files = [cmd]
+
+        seen = set()
+        for dir in path:
+            normdir = os.path.normcase(dir)
+            if not normdir in seen:
+                seen.add(normdir)
+                for thefile in files:
+                    name = os.path.join(dir, thefile)
+                    if _access_check(name, mode):
+                        return name
+        return None
 
 
-if len(sys.argv) == 2 and sys.argv[1] == "install":
-    print("Installing Jupyter notebook extensions in the user home as default")
-    destdir=os.path.join(jupyter_config_dir(),'nbextensions','CesiumWidget')
-    print("The CesiumWidget will be in the user directory %s " % destdir)
-    recursive_overwrite(src='./CesiumWidget/', dest=destdir)
+version_ns = {}
+with open(os.path.join(here, 'CesiumWidget', '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
 
-if "all" in sys.argv:
-    destdir='/usr/local/share/jupyter/nbextensions/'
-    print("The CesiumWidget will be installed for all users in %s needs root access" % destdir)
-    recursive_overwrite(src='./CesiumWidget/', dest=destdir)
-    sys.argv.remove("all")
-
-if "user" in sys.argv:
-    home = expanduser("~")
-    destdir=os.path.join(jupyter_data_dir(),'nbextensions','CesiumWidget')
-    print("The CesiumWidget will be in the user directory %s " % destdir)
-    recursive_overwrite(src='./CesiumWidget/', dest=destdir)
-    sys.argv.remove("user")
-
-
-
-
-
-CLASSIFIERS = """\
-Development Status :: 5 - Production/Stable
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-License :: OSI Approved
-Programming Language :: Python :: 2
-Programming Language :: Python :: 3
-Topic :: Software Development
-Topic :: Scientific/Engineering
-Operating System :: Unix
-Operating System :: MacOS
-"""
-AUTHOR = 'epinux'
-MAJOR = 0
-MINOR = 1
-MICRO = 0
-ISRELEASED = False
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
-
-
-def git_version():
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = "Unknown"
-    #print(GIT_REVISION)
-    return GIT_REVISION
-
-GIT_REVISION = git_version()
-
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'): os.remove('MANIFEST')
-
-def get_version_info():
-    # Adding the git rev number needs to be done inside write_version_py(),
-    # otherwise the import of numpy.version messes up the build under Python 3.
-    FULLVERSION = VERSION
-    #if os.path.exists('.git'):
-    GIT_REVISION = git_version()
-    if not ISRELEASED:
-        FULLVERSION += '.dev-' + GIT_REVISION[:7]
-
-    return FULLVERSION, GIT_REVISION
-
-
-def write_version_py(filename='CesiumWidget/version.py'):
-    cnt = """
-# THIS FILE IS GENERATED FROM CesiumWidget SETUP.PY
-short_version = '%(version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-git_revision = '%(git_revision)s'
-release = %(isrelease)s
-
-if not release:
-    version = full_version
-"""
-    FULLVERSION, GIT_REVISION = get_version_info()
-
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % {'author' : AUTHOR,
-                       'version': VERSION,
-                       'full_version' : FULLVERSION,
-                       'git_revision' : GIT_REVISION,
-                       'isrelease': str(ISRELEASED)})
-    finally:
-        a.close()
-
-
-#with open('README.md') as file:
-#    long_description = file.read()
-
-
-write_version_py()
-
-    
-setup(
-    name = 'CesiumWidget',
-    version = '0.1.0',
-    description = 'A Cesiumjs widget for the IPython notebook',
-    #long_description=long_description,
-    author = 'Massimo Di Stefano, Petrus Hyvonen',
-    #author_unixid = 'epinux',
-    author_email = 'epiesasha@me.com, petrus.hyvonen@gmail.com',
-    url = 'https://github.com/epifanio/CesiumWidget',
-    packages = ['CesiumWidget'],
-    #package_dir = {'': 'lib'},
-    license = 'Apache',
-    platforms = ["Linux", "Mac OS-X", "Unix"],
-    classifiers = [
-        'Development Status :: 1 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache License',
-        'Operating System :: UNIX based systems',
-        'Programming Language :: Python',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development',
-        'Topic :: Scientific/Engineering',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Operating System :: MacOS',
+setup_args = {
+    'name': 'CesiumWidget',
+    'version': version_ns['__version__'],
+    'description': 'A Cesiumjs widget for the IPython notebook.',
+    'long_description': LONG_DESCRIPTION,
+    'License': 'Apache',
+    'include_package_data': True,
+    'install_requires': ['ipywidgets', 'czml'],
+    'packages': find_packages(),
+    'zip_safe': False,
+    'author': 'Massimo Di Stefano',
+    'url': 'https://github.com/epifanio/CesiumWidget',
+    'keywords': [
+        'ipython',
+        'jupyter',
+        'widgets',
+        'graphics',
+        'plotting',
+        'cesiumjs',
     ],
-)
+    'classifiers': [
+        'Development Status :: 1 - Alfa',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Multimedia :: Graphics',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+}
+
+setup(**setup_args)
