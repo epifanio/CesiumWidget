5d8fccb63f7aa3d2a9178b37623d49cb1c7691b1
epifanio <epiesasha@me.com>
2015-12-05 14:57:16 -0500

    still struggling with binder

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER Massimo Di Stefano <epiesasha@me.com>
 USER main
 
 RUN /home/main/anaconda/envs/python3/bin/pip install --upgrade pip
-RUN /home/main/anaconda/envs/python3/bin/pip install -U jupyter
+#RUN /home/main/anaconda/envs/python3/bin/pip install -U jupyter
 RUN /home/main/anaconda/envs/python3/bin/pip install czml
 RUN /home/main/anaconda/envs/python3/bin/pip install geocoder
 
@@ -15,7 +15,7 @@ RUN conda install \
   && pip install \
     czml \
     geocoder
-#RUN /home/main/anaconda/envs/python3/bin/pip install ipywidgets
+RUN /home/main/anaconda/envs/python3/bin/pip install ipywidgets
 #RUN /home/main/anaconda/bin/pip install ipywidgets
 
 USER root
@@ -23,12 +23,6 @@ USER root
 RUN echo "root:root" | chpasswd
 RUN echo "main:main" | chpasswd
 
-#ADD install_cesiumwidget.sh /tmp/install_cesiumwidget.sh
-#RUN /tmp/install_cesiumwidget.sh
-
-#RUN mkdir $HOME/Examples/
-#ADD Examples/ $HOME/Examples/
-#RUN chown -R main:main $HOME/Examples
 
 USER main
 
@@ -38,8 +32,8 @@ RUN git clone https://github.com/epifanio/CesiumWidget --depth=1
 WORKDIR CesiumWidget
 
 RUN python setup.py install
-# jupyter-pip so crazy. this is cheating, as a real user wouldn't have
-# the source checked out...
+RUN /home/main/anaconda/envs/python3/bin/python setup.py install
+
 RUN jupyter nbextension install CesiumWidget --user --quiet
 
 WORKDIR $HOME/Examples
--- setup.py
+++ setup.py
@@ -1,127 +1,34 @@
-from __future__ import print_function
-from setuptools import setup, find_packages, Command
-from setuptools.command.egg_info import egg_info
-from subprocess import check_call
-import os
-import sys
-import platform
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
+readme = open('README.md').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
-LONG_DESCRIPTION = """
-CesiumWidget
-====
-
-A Cesiumjs widget for the Ipython notebook, forked from https://github.com/petrushy/CesiumWidget.
-
-.. _CesiumWidget: https://github.com/epifanio/CesiumWidget
-.. _Cesiumjs: http://cesiumjs.org/
-
-"""
-
-try:
-    from shutil import which
-except ImportError:
-    # The which() function is copied from Python 3.4.3
-    # PSF license version 2 (Python Software Foundation License Version 2)
-    def which(cmd, mode=os.F_OK | os.X_OK, path=None):
-        """Given a command, mode, and a PATH string, return the path which
-        conforms to the given mode on the PATH, or None if there is no such
-        file.
-        `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
-        of os.environ.get("PATH"), or can be overridden with a custom search
-        path.
-        """
-        # Check that a given file can be accessed with the correct mode.
-        # Additionally check that `file` is not a directory, as on Windows
-        # directories pass the os.access check.
-        def _access_check(fn, mode):
-            return (os.path.exists(fn) and os.access(fn, mode)
-                    and not os.path.isdir(fn))
-
-        # If we're given a path with a directory part, look it up directly rather
-        # than referring to PATH directories. This includes checking relative to the
-        # current directory, e.g. ./script
-        if os.path.dirname(cmd):
-            if _access_check(cmd, mode):
-                return cmd
-            return None
-
-        if path is None:
-            path = os.environ.get("PATH", os.defpath)
-        if not path:
-            return None
-        path = path.split(os.pathsep)
-
-        if sys.platform == "win32":
-            # The current directory takes precedence on Windows.
-            if not os.curdir in path:
-                path.insert(0, os.curdir)
-
-            # PATHEXT is necessary to check on Windows.
-            pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
-            # See if the given file matches any of the expected path extensions.
-            # This will allow us to short circuit when given "python.exe".
-            # If it does match, only test that one, otherwise we have to try
-            # others.
-            if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
-                files = [cmd]
-            else:
-                files = [cmd + ext for ext in pathext]
-        else:
-            # On other platforms you don't have things like PATHEXT to tell you
-            # what file suffixes are executable, so just pass on cmd as-is.
-            files = [cmd]
-
-        seen = set()
-        for dir in path:
-            normdir = os.path.normcase(dir)
-            if not normdir in seen:
-                seen.add(normdir)
-                for thefile in files:
-                    name = os.path.join(dir, thefile)
-                    if _access_check(name, mode):
-                        return name
-        return None
-
-
-version_ns = {}
-with open(os.path.join(here, 'CesiumWidget', '_version.py')) as f:
-    exec(f.read(), {}, version_ns)
-
-setup_args = {
-    'name': 'CesiumWidget',
-    'version': version_ns['__version__'],
-    'description': 'A Cesiumjs widget for the IPython notebook.',
-    'long_description': LONG_DESCRIPTION,
-    'License': 'Apache',
-    'include_package_data': True,
-    'install_requires': ['ipywidgets', 'czml'],
-    'packages': find_packages(),
-    'zip_safe': False,
-    'author': 'Petrus Hyvonen <petrus.hyvonen@gmail.com>, Massimo Di Stefano <epiesasha@me.com>',
-    'author_email': 'petrus.hyvonen@gmail.com, epiesasha@me.com',
-    'url': 'https://github.com/epifanio/CesiumWidget',
-    'keywords': [
-        'ipython',
-        'jupyter',
-        'widgets',
-        'graphics',
-        'plotting',
-        'cesiumjs',
-    ],
-    'classifiers': [
-        'Development Status :: 1 - Alfa',
+setup(
+    name='CesiumWidget',
+    version='0.1.0',
+    description='IPython widget for Cesium world globe',
+    long_description=readme + '\n\n' + history,
+    author='Petrus Hyvonen',
+    author_email='petrus.hyvonen@gmail.com',
+    url='https://github.com/petrushy/CesiumWidget',
+    packages=['CesiumWidget'],
+    install_requires=['ipywidgets'],
+    include_package_data=True,
+    license="Apache",
+    zip_safe=False,
+    keywords='CesiumWidget ipython',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Multimedia :: Graphics',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-    ],
-}
-
-setup(**setup_args)
+        'Programming Language :: Python :: 3.4'
+    ]
+)
