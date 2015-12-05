76fd107732e847e4b459739ae1d10a34b2f20832
epifanio <epiesasha@me.com>
2015-12-05 15:58:55 -0500

    remove unused files

--- install_cesiumwidget.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /home/main/src/
-git clone https://github.com/epifanio/CesiumWidget /home/main/src/CesiumWidget
-cd /home/main/src/CesiumWidget
-/home/main/anaconda/envs/python3/bin/python setup.py install
-/home/main/anaconda/envs/python3/bin/python -m CesiumWidget.install --user
-# --symlink --user --force
-
-
